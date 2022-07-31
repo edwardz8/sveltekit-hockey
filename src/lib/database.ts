c3ba83bde1c48789203fb8686cce5875b5a08cba
edwardz8 <tolklorist@gmail.com>
2022-07-31 17:57:23 -0400

    init commit

--- src/lib/database.ts
+++ src/lib/database.ts
@@ -1,10 +1,7 @@
 /* import prisma from '@prisma/client'
  */
 
-import pkg from '@prisma/client';
-const { PrismaClient } = pkg;
-
-// import { PrismaClient } from '@prisma/client'
+import { PrismaClient } from '@prisma/client'
 
 const prisma = new PrismaClient()
 export const db = new PrismaClient()
