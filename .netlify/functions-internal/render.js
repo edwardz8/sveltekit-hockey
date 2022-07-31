35e82d91d7b17780ca1a5c7c3ac37a67ffdb70d4
edwardz8 <tolklorist@gmail.com>
2022-07-31 17:16:39 -0400

    init commit

--- .netlify/functions-internal/render.js
+++ .netlify/functions-internal/render.js
@@ -2,10 +2,10 @@ const { init } = require('../serverless.js');
 
 exports.handler = init({
 	appDir: "_app",
-	assets: new Set(["favicon.png","robots.txt","svelte-welcome.png","svelte-welcome.webp"]),
+	assets: new Set(["_redirects","favicon.png","robots.txt","svelte-welcome.png","svelte-welcome.webp"]),
 	mimeTypes: {".png":"image/png",".txt":"text/plain",".webp":"image/webp"},
 	_: {
-		entry: {"file":"_app/immutable/start-90eec8b9.js","imports":["_app/immutable/start-90eec8b9.js","_app/immutable/chunks/index-10167abb.js","_app/immutable/chunks/index-b5bef26d.js"],"stylesheets":[]},
+		entry: {"file":"_app/immutable/start-10b827fc.js","imports":["_app/immutable/start-10b827fc.js","_app/immutable/chunks/index-10167abb.js","_app/immutable/chunks/index-b5bef26d.js"],"stylesheets":[]},
 		nodes: [
 			() => Promise.resolve().then(() => require('../server/nodes/0.js')),
 			() => Promise.resolve().then(() => require('../server/nodes/1.js')),
